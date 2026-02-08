75dc9755cec57969beda9732005b016f2c5c413f
Stavros Nikoletopoulos <snikoletopoulos@hotmail.com>
2026-02-08 13:41:28 +0100

    Fix commenting in react

--- lua/plugins/text.lua
+++ lua/plugins/text.lua
@@ -54,4 +54,18 @@ return {
 		},
 		opts = {},
 	},
+	{
+		"numToStr/Comment.nvim",
+		enabled = true,
+		dependencies = {
+			{
+				"JoosepAlviste/nvim-ts-context-commentstring",
+				enabled = true,
+				opts = { enable_autocmd = false },
+			},
+		},
+		opts = {
+			pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+		},
+	},
 }
