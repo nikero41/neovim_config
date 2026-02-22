dd3dd1431458fd4d2c093a270dfa0b3fc53007dc
Stavros Nikoletopoulos <snikoletopoulos@hotmail.com>
2026-02-22 03:06:06 +0100

    Format code

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -56,13 +56,8 @@ return {
 					messages = { view_search = false },
 					notify = { enabled = false },
 					lsp = {
-						signature = { enabled = true },
 						hover = { enabled = false },
-						override = {
-							["vim.lsp.util.convert_input_to_markdown_lines"] = false,
-							["vim.lsp.util.stylize_markdown"] = false,
-							["cmp.entry.get_documentation"] = false,
-						},
+						signature = { enabled = false },
 					},
 					views = {
 						cmdline_popup = {
