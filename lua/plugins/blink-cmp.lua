18c4a610c82d6dd3f44d93fb78ab35fdeec6523e
Stavros Nikoletopoulos <snikoletopoulos@hotmail.com>
2026-02-27 00:54:03 +0100

    Remove unneeded field

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -2,7 +2,7 @@
 return {
 	{
 		"L3MON4D3/LuaSnip",
-		dependencies = { { "rafamadriz/friendly-snippets", lazy = true } },
+		dependencies = { "rafamadriz/friendly-snippets" },
 		version = "v2.*",
 		build = "make install_jsregexp",
 		opts = {
