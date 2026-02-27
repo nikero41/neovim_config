ee98cb02df2a274b15c7b05fc1fcfb6b5bf887e3
Stavros Nikoletopoulos <snikoletopoulos@hotmail.com>
2026-02-27 18:06:43 +0100

    Fix javascript filetypes formating

--- lua/plugins/language/tools.lua
+++ lua/plugins/language/tools.lua
@@ -8,22 +8,30 @@ return {
 			{ "<leader>lf", function() require("conform").format({ async = true }) end, desc = "Format file" },
 		},
 		---@module "conform"
-		---@type conform.setupOpts
-		opts = {
-			formatters_by_ft = {
-				bash = { "shfmt" },
-				c = {},
-				javascript = { "prettierd", "prettier", stop_after_first = true },
-				kotlin = { "ktlint" },
-				lua = { "stylua" },
-				markdown = { "prettierd", "prettier", "markdownlint", stop_after_first = true },
-				prisma = { "prisma-fmt" },
-				python = { "isort", "black" },
-				rust = { "rustfmt" },
-				sql = { "sqlfmt" },
-			},
-			default_format_opts = { lsp_format = "fallback" },
-		},
+		---@param opts conform.setupOpts
+		---@return conform.setupOpts
+		opts = function(_, opts)
+			local new_opts = vim.tbl_extend("force", opts, {
+				formatters_by_ft = {
+					bash = { "shfmt" },
+					c = {},
+					kotlin = { "ktlint" },
+					lua = { "stylua" },
+					markdown = { "prettierd", "prettier", "markdownlint", stop_after_first = true },
+					prisma = { "prisma-fmt" },
+					python = { "isort", "black" },
+					rust = { "rustfmt" },
+					sql = { "sqlfmt" },
+				},
+				default_format_opts = { lsp_format = "fallback" },
+			})
+
+			for _, language in ipairs(require("filetypes").javascript) do
+				new_opts.formatters_by_ft[language] = { "prettierd", "prettier", stop_after_first = true }
+			end
+
+			return new_opts
+		end,
 		init = function()
 			vim.api.nvim_create_user_command(
 				"Format",
