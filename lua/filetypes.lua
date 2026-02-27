3a1e07b677124c85f1091e4a88f48d5d55887eeb
Stavros Nikoletopoulos <snikoletopoulos@hotmail.com>
2026-02-27 09:52:24 +0100

    Add extra filetype definitions

--- lua/filetypes.lua
+++ lua/filetypes.lua
@@ -1,3 +1,24 @@
+vim.filetype.add({
+	extension = {
+		env = "dotenv",
+		podspec = "ruby",
+		tmux = "tmux",
+		gitconfig = "gitconfig",
+	},
+	filename = {
+		[".env"] = "dotenv",
+		["tsconfig.json"] = "jsonc",
+		[".eslintrc.json"] = "jsonc",
+		[".yamlfmt"] = "yaml",
+		[".sqlfluff"] = "toml",
+		["Podfile"] = "ruby",
+		["dot-zshrc"] = "zsh",
+	},
+	pattern = {
+		["%.env%.[%w_.-]+"] = "dotenv",
+	},
+})
+
 return {
 	javascript = {
 		"typescript",
