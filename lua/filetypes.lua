0f428afb57cf4b5b3712e6e1eafa6a3585d72cd8
Stavros Nikoletopoulos <snikoletopoulos@hotmail.com>
2026-02-27 11:30:29 +0100

    Add filetype definitions and autocmd

--- lua/filetypes.lua
+++ lua/filetypes.lua
@@ -1,3 +1,15 @@
+local filetypes = {
+	javascript = {
+		"typescript",
+		"typescriptreact",
+		"typescript.tsx",
+		"javascript",
+		"javascriptreact",
+		"javascript.jsx",
+		"jsx",
+	},
+}
+
 vim.filetype.add({
 	extension = {
 		env = "dotenv",
@@ -19,14 +31,39 @@ vim.filetype.add({
 	},
 })
 
-return {
-	javascript = {
-		"typescript",
-		"typescriptreact",
-		"typescript.tsx",
-		"javascript",
-		"javascriptreact",
-		"javascript.jsx",
-		"jsx",
+-- close some filetypes with <q>
+vim.api.nvim_create_autocmd("FileType", {
+	-- TODO: group = augroup("close_with_q"),
+	pattern = {
+		"PlenaryTestPopup",
+		"checkhealth",
+		"dbout",
+		"gitsigns-blame",
+		"grug-far",
+		"help",
+		"lspinfo",
+		"neotest-output",
+		"neotest-output-panel",
+		"neotest-summary",
+		"notify",
+		"qf",
+		"spectre_panel",
+		"startuptime",
+		"tsplayground",
 	},
-}
+	callback = function(event)
+		vim.bo[event.buf].buflisted = false
+		vim.schedule(function()
+			vim.keymap.set("n", "q", function()
+				vim.cmd("close")
+				pcall(vim.api.nvim_buf_delete, event.buf, { force = true }) -- TODO: use snack?
+			end, {
+				buffer = event.buf,
+				silent = true,
+				desc = "Quit buffer",
+			})
+		end)
+	end,
+})
+
+return filetypes
