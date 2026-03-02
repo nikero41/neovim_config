8cfec72fd3b104c186f5b40d0db462a4717db6bf
Stavros Nikoletopoulos <snikoletopoulos@hotmail.com>
2026-03-02 14:02:40 +0200

    Switch to lsp folding if available

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -4,6 +4,21 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 	callback = function() vim.hl.on_yank() end,
 })
 
+-- use LSP folding if available
+vim.api.nvim_create_autocmd({ "BufEnter", "LspAttach" }, {
+	callback = function(args)
+		if vim.wo.foldexpr == "v:lua.vim.lsp.foldexpr()" then return end
+
+		for _, c in ipairs(vim.lsp.get_clients({ bufnr = args.buf })) do
+			if c.server_capabilities and c.server_capabilities.foldingRangeProvider then
+				vim.wo.foldexpr = "v:lua.vim.lsp.foldexpr()"
+				pcall(vim.lsp.buf.refresh_folds, { bufnr = args.buf })
+				return
+			end
+		end
+	end,
+})
+
 -- resize splits if window got resized
 vim.api.nvim_create_autocmd("VimResized", {
 	-- group = augroup("resize_splits"),
