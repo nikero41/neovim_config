6909dba004684f3b841990d318bcfb6d79a90159
Stavros Nikoletopoulos <snikoletopoulos@hotmail.com>
2026-03-02 22:13:36 +0200

    Resolve todo

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -29,8 +29,7 @@ function Autocmds:setup()
 		-- group = augroup("resize_splits"),
 		callback = function()
 			local current_tab = vim.fn.tabpagenr()
-			vim.cmd("tabdo wincmd =")
-			-- TODO: vim.cmd.wincmd({ "=",  })
+			vim.cmd.tabdo("wincmd =")
 			vim.cmd.tabnext(current_tab)
 		end,
 	})
