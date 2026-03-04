5a8111754e727570856cf1125e097fde1bb8ce43
Stavros Nikoletopoulos <snikoletopoulos@hotmail.com>
2026-03-04 20:29:24 +0200

    Remove neo-tree default mappings from configuration

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -70,20 +70,6 @@ return {
 				},
 			},
 			default_component_configs = {
-				--diagnostics = {
-				--  symbols = {
-				--    hint = "H",
-				--    info = "I",
-				--    warn = "!",
-				--    error = "X",
-				--  },
-				--  highlights = {
-				--    hint = "DiagnosticSignHint",
-				--    info = "DiagnosticSignInfo",
-				--    warn = "DiagnosticSignWarn",
-				--    error = "DiagnosticSignError",
-				--  },
-				--},
 				indent = {
 					padding = 0,
 					with_markers = true,
@@ -167,33 +153,14 @@ return {
 					nowait = true,
 				},
 				mappings = {
-					["<space>"] = {
-						"toggle_node",
-						nowait = false, -- disable `nowait` if you have existing combos starting with this char that you want to use
-					},
-					["<2-LeftMouse>"] = "open",
 					["<cr>"] = "open",
 					-- ["<cr>"] = { "open", config = { expand_nested_files = true } }, -- expand nested file takes precedence
-					["<esc>"] = "cancel", -- close preview or floating neo-tree window
-					["P"] = {
-						"toggle_preview",
-						config = {
-							use_float = true,
-							use_snacks_image = true,
-							use_image_nvim = true,
-							-- title = "Neo-tree Preview", -- You can define a custom title for the preview floating window.
-						},
-					},
-					["<C-f>"] = { "scroll_preview", config = { direction = -10 } },
-					["<C-b>"] = { "scroll_preview", config = { direction = 10 } },
-					["l"] = "focus_preview",
 					["S"] = "open_split",
 					-- ["S"] = "split_with_window_picker",
 					["s"] = "open_vsplit",
 					-- ["sr"] = "open_rightbelow_vs",
 					-- ["sl"] = "open_leftabove_vs",
 					-- ["s"] = "vsplit_with_window_picker",
-					["t"] = "open_tabnew",
 					-- ["<cr>"] = "open_drop",
 					-- ["t"] = "open_tab_drop",
 					["w"] = "open_with_window_picker",
@@ -202,7 +169,6 @@ return {
 					["z"] = "close_all_nodes",
 					--["Z"] = "expand_all_nodes",
 					--["Z"] = "expand_all_subnodes",
-					["R"] = "refresh",
 					["a"] = {
 						"add",
 						-- some commands may take optional config options, see `:h neo-tree-mappings` for details
@@ -211,28 +177,11 @@ return {
 						},
 					},
 					["A"] = "add_directory", -- also accepts the config.show_path and config.insert_as options.
-					["d"] = "delete",
-					["r"] = "rename",
-					["y"] = "copy_to_clipboard",
-					["x"] = "cut_to_clipboard",
-					["p"] = "paste_from_clipboard",
-					["<C-r>"] = "clear_clipboard",
-					["c"] = "copy", -- takes text input for destination, also accepts the config.show_path and config.insert_as options
-					["m"] = "move", -- takes text input for destination, also accepts the config.show_path and config.insert_as options
-					["e"] = "toggle_auto_expand_width",
-					["q"] = "close_window",
-					["?"] = "show_help",
-					-- You can sort by command name with:
-					-- ["?"] = { "show_help", config = { sorter = function(a, b) return a.mapping.text < b.mapping.text end } },
-					-- The type of a and b are neotree.Help.Mapping
-					["<"] = "prev_source",
-					[">"] = "next_source",
 				},
 			},
 			filesystem = {
 				window = {
 					mappings = {
-						["H"] = "toggle_hidden",
 						["/"] = "fuzzy_finder",
 						--["/"] = {"fuzzy_finder", config = { keep_filter_on_submit = true }},
 						--["/"] = "filter_as_you_type", -- this was the default until v1.28
@@ -240,21 +189,8 @@ return {
 						-- ["D"] = "fuzzy_sorter_directory",
 						["#"] = "fuzzy_sorter", -- fuzzy sorting using the fzy algorithm
 						["f"] = "filter_on_submit",
-						["<C-x>"] = "clear_filter",
-						["<bs>"] = "navigate_up",
-						["."] = "set_root",
-						["[g"] = "prev_git_modified",
-						["]g"] = "next_git_modified",
 						["i"] = "show_file_details", -- see `:h neo-tree-file-actions` for options to customize the window.
-						["b"] = "rename_basename",
 						["o"] = { "show_help", nowait = false, config = { title = "Order by", prefix_key = "o" } },
-						["oc"] = { "order_by_created", nowait = false },
-						["od"] = { "order_by_diagnostics", nowait = false },
-						["og"] = { "order_by_git_status", nowait = false },
-						["om"] = { "order_by_modified", nowait = false },
-						["on"] = { "order_by_name", nowait = false },
-						["os"] = { "order_by_size", nowait = false },
-						["ot"] = { "order_by_type", nowait = false },
 					},
 					fuzzy_finder_mappings = { -- define keymaps for filter popup window in fuzzy_finder_mode
 						["<down>"] = "move_cursor_down",
