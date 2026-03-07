6c7f5b3166ea71b3ee02805dc0039830dca9da4c
Stavros Nikoletopoulos <snikoletopoulos@hotmail.com>
2026-03-07 14:30:48 +0200

    Format code

--- init.lua
+++ init.lua
@@ -18,14 +18,10 @@ vim.opt.rtp:prepend(lazypath)
 
 local lazy_loaded, lazy = pcall(require, "lazy")
 if not lazy_loaded then
-	vim.api.nvim_echo(
-		{
-			{ ("Unable to load lazy from: %s\n"):format(lazypath), "ErrorMsg" },
-			{ "Press any key to exit...", "MoreMsg" },
-		},
-		true,
-		{}
-	)
+	vim.api.nvim_echo({
+		{ ("Unable to load lazy from: %s\n"):format(lazypath), "ErrorMsg" },
+		{ "Press any key to exit...", "MoreMsg" },
+	}, true, {})
 	vim.fn.getchar()
 	vim.cmd.quit()
 end
