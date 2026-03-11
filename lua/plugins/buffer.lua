c46c667cbd86e3b03e1c0463b991c875f7a9dcca
Stavros Nikoletopoulos <snikoletopoulos@hotmail.com>
2026-03-11 22:19:59 +0100

    Do not fold by default

--- lua/plugins/buffer.lua
+++ lua/plugins/buffer.lua
@@ -80,6 +80,7 @@ return {
 		opts = {
 			closeOnlyOnFirstColumn = false, -- `h` and `^` only fold in the 1st column
 			scrollLeftOnCaret = false, -- `^` should scroll left (basically mapped to `0^`)
+			autoFold = { enabled = false },
 		},
 	},
 }
