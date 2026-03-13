04fc132ab069db2db4c016e85daf996182978043
Stavros Nikoletopoulos <snikoletopoulos@hotmail.com>
2026-03-13 23:54:00 +0100

    Properly wait for git command

--- init.lua
+++ init.lua
@@ -11,8 +11,16 @@ if not vim.uv.fs_stat(lazypath) then
 		"https://github.com/folke/lazy.nvim.git",
 		"--branch=stable",
 		lazypath,
-	})
-	if vim.v.shell_error ~= 0 then error("Error cloning lazy.nvim:\n" .. out) end
+	}):wait()
+	if out.code ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out.stderr, "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
 end
 vim.opt.rtp:prepend(lazypath)
 
