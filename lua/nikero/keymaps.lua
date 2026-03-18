c9a7c620b2b1cb6a3d212d8f1f4e47ea6244c3e7
Stavros Nikoletopoulos <snikoletopoulos@hotmail.com>
2026-03-19 00:19:19 +0100

    Fix keymaps errors

--- lua/nikero/keymaps.lua
+++ lua/nikero/keymaps.lua
@@ -40,7 +40,8 @@ function Keymaps:setup()
 	for _, keymap in ipairs(self.keymaps) do
 		local mode, lhs, rhs, opts = keymap[1], keymap[2], keymap[3], (keymap[4] or {})
 
-		if opts.unique == nil then opts.unique = true end
+		local can_set_unique = opts.lsp == nil and opts.ft == nil
+		if opts.unique == nil and can_set_unique then opts.unique = true end
 		---@type Keymap.Opts
 		opts = vim.tbl_extend("force", self.opts, opts)
 
