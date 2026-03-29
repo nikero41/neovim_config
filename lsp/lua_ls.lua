e6301edb364ce33308612347a9ea08a74b299ee6
Stavros Nikoletopoulos <snikoletopoulos@hotmail.com>
2026-03-29 15:00:25 +0200

    Let lazydev handle this

--- lsp/lua_ls.lua
+++ lsp/lua_ls.lua
@@ -22,12 +22,6 @@ return {
 				version = "LuaJIT",
 				path = { "lua/?.lua", "lua/?/init.lua", "lsp/?.lua" },
 			},
-			workspace = {
-				checkThirdParty = true,
-				-- -- NOTE: this is a lot slower and will cause issues when working on your own configuration.
-				-- --  See https://github.com/neovim/nvim-lspconfig/issues/3189
-				-- library = vim.api.nvim_get_runtime_file("", true),
-			},
 		})
 	end,
 }
