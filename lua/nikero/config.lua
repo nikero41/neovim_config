f8c9e40e1b090188a0935bd7aa05ffec3ab5640a
Stavros Nikoletopoulos <snikoletopoulos@hotmail.com>
2026-04-06 03:44:46 +0200

    Document config

--- lua/nikero/config.lua
+++ lua/nikero/config.lua
@@ -1,4 +1,6 @@
 ---@class Config
+---@field transparency boolean
+---@field hide_code_lens_ft string[] Filetypes where code lens should be disabled
 local Config = {
 	transparency = false,
 	hide_code_lens_ft = { "lua" },
