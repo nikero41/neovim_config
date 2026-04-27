4663373e1da1f0ba971f36ac74ebd30f60e84d97
Stavros Nikoletopoulos <snikoletopoulos@hotmail.com>
2026-04-28 01:09:37 +0200

    Remove codelens from rust files

--- lua/nikero/config.lua
+++ lua/nikero/config.lua
@@ -3,7 +3,7 @@
 ---@field hide_code_lens_ft string[] Filetypes where code lens should be disabled
 local Config = {
 	transparency = false,
-	hide_code_lens_ft = { "lua" },
+	hide_code_lens_ft = { "lua", "rust" },
 }
 
 return Config
