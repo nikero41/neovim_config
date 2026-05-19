298506e6a3179b053fa0994cfbbe9a171bfaff91
Stavros Nikoletopoulos <snikoletopoulos@hotmail.com>
2026-05-19 23:31:46 +0200

    Remove old config for sqls

--- lua/plugins/language/sql.lua
+++ lua/plugins/language/sql.lua
@@ -1,16 +1,4 @@
 ---@type LazySpec
 return {
-	{
-		"nanotee/sqls.nvim",
-		init = function()
-			vim.api.nvim_create_autocmd("LspAttach", {
-				desc = "Load sqls.nvim with sqls",
-				group = vim.api.nvim_create_augroup("sqls.nvim", { clear = true }),
-				callback = function(args)
-					local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
-					if client.name == "sqls" then require("sqls").on_attach(client, args.buf) end
-				end,
-			})
-		end,
-	},
+	{ "nanotee/sqls.nvim" },
 }
