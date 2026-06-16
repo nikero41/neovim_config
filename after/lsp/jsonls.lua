e62fc43012cc8ef4df56cb2b30997809a5b2b776
Stavros Nikoletopoulos <snikoletopoulos@hotmail.com>
2026-06-16 18:03:01 +0300

    Disable jsonls formatting

--- after/lsp/jsonls.lua
+++ after/lsp/jsonls.lua
@@ -1,5 +1,8 @@
 ---@type vim.lsp.Config | { settings?: lsp.jsonls }
 return {
+	init_options = {
+		provideFormatter = false,
+	},
 	settings = {
 		json = {
 			schemas = require("schemastore").json.schemas(),
